0899931bcec8a703a66ce6bc9128ca3cbbf422bb
Asa-Is-My-Name <97054530+Asa-Is-My-Name@users.noreply.github.com>
2023-03-09 10:20:07 -0800

    Update task.py

--- task.py
+++ task.py
@@ -49,9 +49,8 @@ def conv_endian(num, endian='big'):
         return None
     byte_list = []
     negative = False
-    hex_table ={0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8',
-                9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E',
-                15: 'F'}
+    hex_table = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8',
+                9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
     remainder = -1
     if num < 0:
         negative = True
