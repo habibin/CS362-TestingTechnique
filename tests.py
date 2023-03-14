c8c872343c6587368007c1c8f712ffa311528b38
mathcodeman <Hins.Guo@ibm.com>
2023-03-14 09:23:48 +0800

    fixed linting issue

--- tests.py
+++ tests.py
@@ -7,7 +7,8 @@ import unittest
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        # tests the hexadecimals in conv_num() function with correct hexadecimals uppercase, lowercase, and negative #
+        # tests the hexadecimals in conv_num() function with correct
+        # hexadecimals uppercase, lowercase, and negative #
         for count in range(100):
             # creates a random correct positive hex string
             random_pos_hex = hex(random.randint(0, 100000))
@@ -20,22 +21,28 @@ class TestCase(unittest.TestCase):
 
             # creates a hexadecimal with a decimal in it.
             string_length = len(choice1)
-            dec_position = random.randint(0, string_length + 1)
-            dec_hex = choice1[:dec_position] + '.' + choice1[dec_position:]
+            dec_pos = random.randint(0, string_length + 1)
+            dec_hex = choice1[:dec_pos] + '.' + choice1[dec_pos:]
 
-            # creates hexadecimals that contain letters greater than f, uppercase or lowercase.
+            # creates hexadecimals that contain letters greater than f,
+            # uppercase or lowercase.
             letters = ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
             # selects a random lowercase or uppercase letter
-            letter = random.choice([random.choice(letters), random.choice(letters).upper()])
+            letter = random.choice([random.choice(letters),
+                                    random.choice(letters).upper()])
             # inserts the letter at random location in the hexadecimal.
-            letter_hex = choice1[:dec_position] + letter + choice1[dec_position:]
+            letter_hex = choice1[:dec_pos] + letter + choice1[dec_pos:]
 
             # random choice chooses one of the options randomly to test
-            rand_choice = random.choice([random_pos_hex.upper(), random_pos_hex.lower(), random_neg_hex.upper(),
-                                         random_neg_hex.lower(), dec_hex, letter_hex])
+            rand_choice = random.choice([random_pos_hex.upper(),
+                                         random_pos_hex.lower(),
+                                         random_neg_hex.upper(),
+                                         random_neg_hex.lower(),
+                                         dec_hex, letter_hex])
 
-            print(rand_choice, type(rand_choice), conv_num(rand_choice), type(conv_num(rand_choice)))
+            print(rand_choice, type(rand_choice),
+                  conv_num(rand_choice), type(conv_num(rand_choice)))
 
     def test2(self):
         # tests float numbers as strings
@@ -47,20 +54,22 @@ class TestCase(unittest.TestCase):
 
             # inserts a decimal in the float.
             string_length = len(random_float)
-            dec_position = random.randint(0, string_length + 1)
-            dec_float = random_float[:dec_position] + '.' + random_float[dec_position:]
+            dec_pos = random.randint(0, string_length + 1)
+            dec_float = random_float[:dec_pos] + '.' + random_float[dec_pos:]
 
             # inserted a letter at random position
             choice1 = random.choice([random_float, random_int])
             string_length = len(choice1)
-            dec_position = random.randint(0, string_length + 1)
+            dec_pos = random.randint(0, string_length + 1)
             letter = random.choice(string.ascii_letters)
-            letter_number = choice1[:dec_position] + letter + choice1[dec_position:]
+            letter_number = choice1[:dec_pos] + letter + choice1[dec_pos:]
 
             # random choice chooses one of the options randomly to test
-            rand_choice = random.choice([random_float, dec_float, random_int, letter_number, '', 1])
+            rand_choice = random.choice([random_float, dec_float, random_int,
+                                         letter_number, '', 1])
 
-            print(rand_choice, type(rand_choice), conv_num(rand_choice), type(conv_num(rand_choice)))
+            print(rand_choice, type(rand_choice),
+                  conv_num(rand_choice), type(conv_num(rand_choice)))
 
     # Test Con_endian funtionality
     def test_zero(self):
